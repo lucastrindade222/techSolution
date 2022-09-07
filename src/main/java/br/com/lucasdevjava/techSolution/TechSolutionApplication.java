21056e580807eac1351c7a509424e9493b3c05a5
lucas <lucas500000000@gmail.com>
2022-09-07 11:53:54 -0300

    Configuration 0.1
    1.Configuration of MySql and phpmyadmin docker images. 2. 2.Configuration of MySql database connections.

--- src/main/java/br/com/lucasdevjava/techSolution/TechSolutionApplication.java
+++ src/main/java/br/com/lucasdevjava/techSolution/TechSolutionApplication.java
@@ -8,6 +8,6 @@ public class TechSolutionApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TechSolutionApplication.class, args);
+		System.out.println("\n\n   α  Não seja tão apressado em julgar os outros, nem os mais sábios conseguem ver o quadro todo.'Gandalf J. R. R. Tolkien' ω");
 	}
-
 }
