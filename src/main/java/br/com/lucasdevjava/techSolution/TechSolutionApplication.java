09d23e54911c8f41bb7bb37a746342ced1ead541
lucas <lucas500000000@gmail.com>
2022-09-07 10:30:57 -0300

    Project creation
    1.criação do projeto

---
+++ src/main/java/br/com/lucasdevjava/techSolution/TechSolutionApplication.java
@@ -0,0 +1,13 @@
+package br.com.lucasdevjava.techSolution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechSolutionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechSolutionApplication.class, args);
+	}
+
+}
